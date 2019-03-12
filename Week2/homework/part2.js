4ed02eddc32ef24d0dec9b5e86d639d020f7929c
Şahin Arslan <42807855+humblecoder00@users.noreply.github.com>
2019-03-12 20:10:08 +0100

    Create part2.js

---
+++ Week2/homework/part2.js
@@ -0,0 +1,70 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "hyfuser",
+  password: "hyfpassword",
+  database: "new_world"
+});
+
+connection.connect();
+console.log("Connected to MySQL");
+
+let arg = process.argv;
+let queryCommand = arg[2];
+let userCode = arg[3];
+let userLang = arg[4];
+
+function insertQuery(...values) {
+  let userInput = console.log("There are more than 10 languages!");
+  let allowedInput = console.log("Entry updated");
+  let triggerQuery =
+    "CREATE TRIGGER ?? AFTER INSERT ON ?? FOR EACH ROW BEGIN IF NEW.?? (COUNT >= 10) THEN SET ?? = ?; ELSE SET ?? = ?; END IF; INSERT INTO ?? (??)";
+  let formatParams = [
+    "language",
+    "countrylanguage",
+    "language",
+    userInput,
+    "countrylanguage",
+    allowedInput,
+    "countrycode",
+    "language",
+    [...values]
+  ];
+
+  triggerQuery = mysql.format(triggerQuery, formatParams);
+  console.log(triggerQuery);
+
+  connection.query(queryInsert, function(error, results, fields) {
+    if (error) throw error;
+    return console.log(results);
+  });
+}
+
+if (queryCommand == "insert") {
+  insertQuery([userCode, userLang]);
+  console.log("success - insert");
+}
+
+/* MYSQL syntax:
+
+delimiter;;
+create procedure new_world.language_add (in_name varchar(52), in lang varchar(30), inoffical enum(‘T’, ‘F’), in _pct float(4,1))
+begin
+declare v_country_code char(3);
+declare v_new_lang_count int;
+select code into v_country_code from country where name = in_name;
+insert into countrylanguage( countrycode, language, isofficial, percentage)
+values
+(v_country_code, in_lang, in_official, in_pct);
+select(language) into v_new_lang_count
+from countrylanguage where countrycode = v_country_code;
+if v_new_lang_count >= 10
+then
+select concat(‘Warning: country’, in_name, ‘now has’, v_new_lang_count, ‘languages’);
+else
+select concat(‘Succeded: country’, in_name, ‘now has’, v_new_lang_count, ‘languages’);
+end if;
+end;;
+
+*/
